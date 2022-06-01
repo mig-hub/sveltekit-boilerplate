cf3dd7b425c9b7317f365e120fdda146c0185ee6
Mickael Riga <mickael.riga.79@gmail.com>
2022-06-01 15:59:04 +0200

    Add slugify helper

---
+++ src/lib/slugify.js
@@ -0,0 +1,13 @@
+// Simple function to create slugs from any string, even with accents
+
+export default text =>
+  text
+    .toString()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-')
+
