2bde7c7ca66e46b0f16fbfd218150b439c973ab7
Mickael Riga <mickael.riga.79@gmail.com>
2022-06-02 10:49:38 +0200

    Add basics for a holding page

---
+++ src/routes/holding@blank.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+  <title>Coming Soon - Company Name</title>
+  <meta name="description" content="">
+</svelte:head>
+
+<div class="holding">
+</div>
+
+<script>
+</script>
+
+<style>
+  .holding {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+  }
+</style>
+
